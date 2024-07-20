00adafd1384fcc4c37cc426fa9686e79bd594548
Sang Shin <sangshinpassion@gmail.com>
2024-07-20 15:07:57 -0400

    more 03 chagnes

--- src/main/java/_03_looping_and_branching/_1_looping/_3_Nested.java
+++ src/main/java/_03_looping_and_branching/_1_looping/_3_Nested.java
@@ -13,24 +13,12 @@ public class _3_Nested {
         // 4 8 12 16 20
         // 5 10 15 20 25
 
+        // TODO-looping-08
+        // - Use nested for loops to print all the
+        //   elements of a 2D array.
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     }
 }
-
-/*
-        Exercise: Print a multiplication table for numbers 1 to 5 using nested for loops.
-
-        1 2 3 4 5
-        2 4 6 8 10
-        3 6 9 12 15
-        4 8 12 16 20
-        5 10 15 20 25
- */
-
-
-/*
-        Exercise: Use nested for loops to print all the elements of a 2D array.
-        e.g. int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
- */
 
 
 
