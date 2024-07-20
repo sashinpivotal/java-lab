5404ca537f003f7fb31c4679764db46a93d45e67
Sang Shin <sangshinpassion@gmail.com>
2024-07-20 14:44:59 -0400

    changes in _01_java_intro

--- src/main/java/_01_java_intro/Main.java
+++ src/main/java/_01_java_intro/Main.java
@@ -8,7 +8,6 @@ public class Main {
         // - Open terminal window: either GitBash or
         //   terminal window within the IntelliJ
         // - Type "java -version"
-        // - Type "javac -version"
 
         // TODO-IDE-01
         // - Run this app using shortcut key "CTRL+SHIFT+R"
@@ -17,15 +16,28 @@ public class Main {
         // TODO-IDE-02:
         // You are going to configure IntelliJ so that
         // you can display Javadoc of all JDK classes.
-        // - Download JDK 11 javadoc zip file
+        // - Download and unzip JDK 11 javadoc zip file
         //   from https://www.oracle.com/java/technologies/javase-jdk11-doc-downloads.html
-        // - Configure JDK documentation so that
-        //   you can display Javadoc of a class
-        //   in a context-sensitive fashion
+        // - Configure JDK documentation to your IntelliJ
+        //   so that you can display Javadoc of a class
+        //   in a context-sensitive fashion:
+        //   - Select File -> Project Structure
+        //   - Select Platform Settings -> SDKs -> 11 -> Documentation Paths
+        //   - Click + sign and add ./docs/api of
+        //     downloaded and unzipped path
+        // - If you want to display Javadoc of "System"
+        //   class in a context sensitive fashion, do
+        //   the following:
+        //   - move your cursor to "System" and press
+        //     SHIFT+F1 and you will see the Javadoc
+        //     of "System" class gets displayed in the
+        //     default browser of your system
         //   (Instructor will demonstrate how it is done.)
+        System.out.println("This is another line in the program");
 
         // TODO-IDE-03
         // - Click "TO DO" at the bottom of the IntelliJ
         //   to display all exercise tasks to be performed
+        //   (Instructor will demonstrate how it is done.)
     }
 }
