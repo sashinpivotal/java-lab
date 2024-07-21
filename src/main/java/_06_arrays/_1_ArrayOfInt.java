917e8a7f066d1c88b77091c6cd0dc1ca97ee8aea
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 19:16:45 -0400

    arrayOfInt solution added

--- src/main/java/_06_arrays/_1_ArrayOfInt.java
+++ src/main/java/_06_arrays/_1_ArrayOfInt.java
@@ -18,16 +18,35 @@ public class _1_ArrayOfInt {
         // - Compute the sum and average value of the "ints"
         //   array
         // - Print the result
+        int sum = 0;
+        int average = 0;
+        for (int value : ints) {
+            sum += value;
+        }
+        average = sum / ints.length;
+        System.out.println("sum = " + sum + ", average = " + average);
 
         // TODO-arrays-02
         // - Find the largest number from the "ints" array
         // - Print the result
+        int largestNumber = Integer.MIN_VALUE;
+        for (int value: ints) {
+            if (value > largestNumber) {
+                largestNumber = value;
+            }
+        }
+        System.out.println("largest number = " + largestNumber);
 
         // TODO-arrays-03
         // - Change each number in the array by multiplying
         //   by 2. In other words, the "ints" array
         //   should have {6, 18, 12, 6, 2, 20, 10}
         // - Display the array before and after
+        System.out.println("Before: " + Arrays.toString(ints));
+        for (int index = 0; index < ints.length; index++) {
+            ints[index] *= 2;
+        }
+        System.out.println("After: " + Arrays.toString(ints));
 
     }
 }
