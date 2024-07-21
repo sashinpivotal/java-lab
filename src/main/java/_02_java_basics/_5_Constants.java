7e25ce849dae66ded72db48d417f4c9af58e47f1
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 17:15:22 -0400

    5_constants added

--- src/main/java/_02_java_basics/_5_Constants.java
+++ src/main/java/_02_java_basics/_5_Constants.java
@@ -1,16 +1,25 @@
 package _02_java_basics;
 
 public class _5_Constants {
+
+    // TODO-constants-01
+    // - Declare a constant named MY_PI of double type
+    //   and assign a value of Math.PI
+    static final double MY_PI = Math.PI;
+
+    // TODO-constants-02
+    // - Declare a constant named MY_CONSTANT_VALUE of int
+    //   type and assign a value of 100 to it.
+    static final int MY_CONSTANT_VALUE = 100;
+
     public static void main(String[] args) {
 
-        // TODO-constants-01
-        // - Declare a constant named MY_PI and assign a value to it.
-        // - Then print its value.
-
-        // TODO-constants-02
-        // - Declare a constant named MY_CONSTANT_VALUE and assign
-        //   a value of 100 to it.
+        // TODO-constants-03
+        // - Print the values of MY_PI and MY_CONSTANT_VALUE
+        System.out.println(MY_PI + ", " + MY_CONSTANT_VALUE);
         // - Try to change value and see what happens.
+        // MY_CONSTANT_VALUE = 400;
+
     }
 }
 
