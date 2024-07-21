6e97aede506eca37d48777f0116212bba1a5d863
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 17:05:46 -0400

    4_primitives changed a bit

--- src/main/java/_02_java_basics/_4_Primitives.java
+++ src/main/java/_02_java_basics/_4_Primitives.java
@@ -4,12 +4,17 @@ public class _4_Primitives {
     public static void main(String[] args) {
 
         // TODO-primitives-01
-        // - Create a char variable with a value of 'A',
-        //   increment it by 1, and
-        // - Print the result.
+        // - Create a char variable called "my_char" with
+        //   a value of 'A',
+        // - Increment it by 1 using three different schemes below
+        //   - my_char++
+        //   - my_char += 1;
+        //   - my_char = my_char + 1;
+        // - Find out which schemes work and display the result
 
         // TODO-primitives-02
-        // - Create a boolean variable and print its inverse.
+        // - Create a boolean variable called "my_boolean" set
+        //   with true and print its inverse.
     }
 }
 
