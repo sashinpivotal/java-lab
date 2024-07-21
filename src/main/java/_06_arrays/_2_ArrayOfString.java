fd29f00909d9753c5cff2ea1c4bc5587f507296a
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 19:28:37 -0400

    2_arrayOfString solution added

--- src/main/java/_06_arrays/_2_ArrayOfString.java
+++ src/main/java/_06_arrays/_2_ArrayOfString.java
@@ -18,6 +18,11 @@ public class _2_ArrayOfString {
         // - Display only elements of familyMembers array
         //   that contains valid family name like following
         //   Dan, Mary, Tom
+        for (String familyMember : familyMembers) {
+            if (familyMember != null) {
+                System.out.println(familyMember);
+            }
+        }
 
         // TODO-arrays-05: conversion of String number to int
         // - Given any array of String called myStringNumbers below
@@ -27,6 +32,12 @@ public class _2_ArrayOfString {
         // - Remember "Integer" class has a method to convert
         //   a String to "int" value
         // - Display both arrays using Arrays.toString(..) method
+        int[] myIntNumbers = new int[myStringNumbers.length];
+        for (int index = 0; index < myStringNumbers.length; index++) {
+            myIntNumbers[index] = Integer.parseInt(myStringNumbers[index]);
+        }
+        System.out.println("myStringNumbers = " + Arrays.toString(myStringNumbers));
+        System.out.println("myIntNumbers = " + Arrays.toString(myIntNumbers));
 
         // TODO-arrays-06: equality testing of array
         // - Given the following three arrays of String
@@ -38,19 +49,23 @@ public class _2_ArrayOfString {
         //   and "myStringArrayB" using
         //   <array-object>.equals method like following
         //   and display the result
-        // boolean equals1 = myStringArrayA.equals(myStringArrayB);
+         boolean equals1 = myStringArrayA.equals(myStringArrayB);
 
         // - Check the equality of the two arrays of "myStringArrayA"
         //   and "myStringArrayB" using
         //   equals method of Arrays utility class like following
         //   and display the result
-        // boolean equals2 = Arrays.equals(myStringArrayA, myStringArrayB);
+         boolean equals2 = Arrays.equals(myStringArrayA, myStringArrayB);
 
         // - Check the equality of the two arrays of "myStringArrayA"
         //   and "myStringArrayC" using
         //   equals method of Arrays utility class like following
         //   and display the result
-        // boolean equals3 = Arrays.equals(myStringArrayA, myStringArrayC);
+         boolean equals3 = Arrays.equals(myStringArrayA, myStringArrayC);
+
+        System.out.println("equals1 = " + equals1);
+        System.out.println("equals2 = " + equals2);
+        System.out.println("equals3 = " + equals3);
 
     }
 }
