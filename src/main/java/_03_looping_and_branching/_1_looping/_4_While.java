d5992274d7a4601aff9202ce6c51e8b2c43f28e8
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 17:54:23 -0400

    while loop solution added

--- src/main/java/_03_looping_and_branching/_1_looping/_4_While.java
+++ src/main/java/_03_looping_and_branching/_1_looping/_4_While.java
@@ -6,14 +6,36 @@ public class _4_While {
         // TODO-looping-09
         // - Use a while loop to print numbers
         //   from 1 to 10
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
 
         // TODO-looping-10
         // - Use a do-while loop to print numbers
         //   from 1 to 10
+        int i2 = 1;
+        do {
+            System.out.print(i2 + " ");
+            i2++;
+        }
+        while (i2 <= 10);
+        System.out.println();
 
         // TODO-looping-11
         // - Use a while loop to find the first number
         //   larger than 100 that's divisible by 7.
+        int i3 = 0;
+        boolean found = false;
+        while (!found) {
+            i3++;
+            if (i3 > 100 && i3 % 7 == 0) {
+                found = true;
+            }
+        }
+        System.out.println(i3);
     }
 }
 
