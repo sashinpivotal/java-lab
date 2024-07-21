af3b9737865d93055714bdde0a1e93a35e4e7865
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 16:45:05 -0400

    solution to 1_variables added

--- src/main/java/_02_java_basics/_1_Variables.java
+++ src/main/java/_02_java_basics/_1_Variables.java
@@ -24,11 +24,16 @@ public class _1_Variables {
         //   and assign your name to it.
         // - Print out a message saying "Hello, my name is ____"
         //   with your name in the blank.
+        String name = "Sang Shin";
+        System.out.println("Hello, my name is " + name);
 
         // TODO-variables-02:
         // - Create two int type variables a and b.
         // - Assign some values to them and print out
         //   the sum of a and b.
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of two integers = " + (a+b));
 
         // TODO-variables-03:
         // - Create two int variables x and y.
@@ -36,6 +41,13 @@ public class _1_Variables {
         // - Then, swap the values of x and y, using the
         //   third variable: int tmp;
         // - Print the values of x and y before and after.
+        int x = 10;
+        int y = 20;
+        System.out.println("Before swapping: x = " + x + ", y = " + y);
+        int tmp = x;
+        x = y;
+        y = tmp;
+        System.out.println("After swapping: x = " + x + ", y = " + y);
 
     }
 }
