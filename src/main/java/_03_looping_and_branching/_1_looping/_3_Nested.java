97e25152afd8ebd5fe17a01d9f7bba5355971ba2
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 17:41:12 -0400

    some changes

--- src/main/java/_03_looping_and_branching/_1_looping/_3_Nested.java
+++ src/main/java/_03_looping_and_branching/_1_looping/_3_Nested.java
@@ -12,13 +12,25 @@ public class _3_Nested {
         // 3 6 9 12 15
         // 4 8 12 16 20
         // 5 10 15 20 25
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
 
         // TODO-looping-08
         // - Use nested for loops to print all the
-        //   elements of a two-dimensional array.
+        //   elements of a two-dimensional array
         int[][] matrix = { {1, 2, 3},
                            {4, 5, 6},
                            {7, 8, 9} };
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
