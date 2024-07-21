de75074d61cc04ae6981e0682ad2caae47c78df0
Sang Shin <sangshinpassion@gmail.com>
2024-07-21 18:43:36 -0400

    switch solution added

--- src/main/java/_03_looping_and_branching/_2_conditionals/iii_switch/SwitchCase_1.java
+++ src/main/java/_03_looping_and_branching/_2_conditionals/iii_switch/SwitchCase_1.java
@@ -11,7 +11,33 @@ public class SwitchCase_1 {
         // - Run the program a few times,
         //   each time changing the `day` value.
         int day = 3;
-
+        String dayOfTheWeek;
+        switch (day) {
+            case 1:
+                dayOfTheWeek = "Monday";
+                break;
+            case 2:
+                dayOfTheWeek = "Tuesday";
+                break;
+            case 3:
+                dayOfTheWeek = "Wednesday";
+                break;
+            case 4:
+                dayOfTheWeek = "Thursday";
+                break;
+            case 5:
+                dayOfTheWeek = "Friday";
+                break;
+            case 6:
+                dayOfTheWeek = "Saturday";
+                break;
+            case 7:
+                dayOfTheWeek = "Sunday";
+                break;
+            default:
+                dayOfTheWeek = "Invalid";
+        }
+        System.out.println(dayOfTheWeek);
     }
 }
 
--- src/main/java/_03_looping_and_branching/_2_conditionals/iii_switch/SwitchCase_2.java
+++ src/main/java/_03_looping_and_branching/_2_conditionals/iii_switch/SwitchCase_2.java
@@ -1,5 +1,7 @@
 package _03_looping_and_branching._2_conditionals.iii_switch;
 
+import java.util.Scanner;
+
 public class SwitchCase_2 {
     public static void main(String[] args) {
         // TODO-conditionals-06
@@ -8,6 +10,23 @@ public class SwitchCase_2 {
         // - Print the result.
         // - Run the program a few times,
         //   each time changing the `a` value.
+        char my_char = 'a';
+        switch (my_char) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("It is a Vowel");
+                break;
+            default:
+                System.out.println("It is Consonant");
+        }
     }
 }
 
