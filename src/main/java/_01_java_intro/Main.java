b306a2d4eea94a6f32cd1d24379fb52526b77338
Sang Shin <sangshinpassion@gmail.com>
2024-07-23 09:57:22 -0400

    more change

--- src/main/java/_01_java_intro/Main.java
+++ src/main/java/_01_java_intro/Main.java
@@ -12,6 +12,7 @@ public class Main {
         // TODO-IDE-01: shortcut keys for running an app
         // - Run this app using shortcut key "CTRL+SHIFT+F10"
         // - Rerun the app using shortcut key "SHIFT+F10" thereafter
+        // - For useful shortcut keys, see https://github.com/sashinpivotal/java-tips
 
         // TODO-IDE-02: context-sensitive Javadoc
         // You are going to configure IntelliJ so that
