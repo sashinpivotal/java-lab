a6a0f0e86db50d6b23ef3f4388be801b5cc43f93
Sang Shin <sangshinpassion@gmail.com>
2024-07-24 18:54:11 -0400

    employee interface exercise added

---
+++ src/main/java/_11_interface/employee_example/HourlyEmployee.java
@@ -0,0 +1,36 @@
+package _11_interface.employee_example;
+
+import java.time.LocalDate;
+
+// TODO-interface-05
+// - Implement TaxPayer interface
+// - Use Tax rate defined for HourlyEmployee
+//   in the TaxPayer interface
+public class HourlyEmployee extends Employee {
+
+    private int numberOfHoursWorkedPerMonth;
+    private double hourlyRate;
+
+    public HourlyEmployee(String name,
+                          LocalDate hireDate,
+                          int numberOfHoursWorkedPerMonth,
+                          double hourlyRate) {
+        super(name, hireDate);
+        setHourlyRate(hourlyRate);
+        setNumberOfHoursWorkedPerMonth(numberOfHoursWorkedPerMonth);
+    }
+
+    public void setNumberOfHoursWorkedPerMonth(int numberOfHoursWorkedPerMonth) {
+        this.numberOfHoursWorkedPerMonth = numberOfHoursWorkedPerMonth;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double computeCompensation() {
+        return numberOfHoursWorkedPerMonth * hourlyRate;
+    }
+
+}
