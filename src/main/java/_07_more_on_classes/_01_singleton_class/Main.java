0684775636c83c18c2e1be8a4dff6ffe009bae6b
Sang Shin <sangshinpassion@gmail.com>
2024-07-24 11:09:12 -0400

    module 7 module conflict resolved

---
+++ src/main/java/_07_more_on_classes/_01_singleton_class/Main.java
@@ -0,0 +1,22 @@
+package _07_more_on_classes._01_singleton_class;
+
+public class Main {
+
+    // TODO-singleton-01
+    // - Create a singleton class called IRS
+    //   (In other words, there should be only
+    //    a single instance of IRS class)
+    // - Add a method called getIRSInstance(),
+    //   which does
+    //   - if IRS object was not created before,
+    //     it will create one and return
+    //   - otherwise, it will return the
+    //     previously created IRS object
+    // - In this main method, call getIRSInstance()
+    //   twice and write code to verify that
+    //   the returned objects are the same object
+    //   instance
+    // - Think about if the default constructor
+    //   of the IRS should be private or not
+
+}
--- src/main/java/_07_more_on_classes/_02_static_methods/Main.java
+++ src/main/java/_07_more_on_classes/_02_static_methods/Main.java
@@ -1,4 +1,4 @@
-package _07_more_on_classes._02_static_method;
+package _07_more_on_classes._02_static_methods;
 
 // TODO-static-method_01
 // - Study how you use static method
