c1c9610e299b8a2f06bdd9b910894316be4c2d47
Sang Shin <sangshinpassion@gmail.com>
2024-07-24 10:38:09 -0400

    overloaded method exercise is added

---
+++ src/main/java/_07_more_on_classes/_03_overloaded_methods/Main.java
@@ -0,0 +1,21 @@
+package _07_more_on_classes._03_overloaded_methods;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // TODO-overloaded-methods-01
+        // - Create a class called Account with the following
+        //   fields
+        //   - name
+        //   - balance
+        // - Add overloaded methods to Account class
+        //   with method names like
+        //   deposit(..) and withdraw(..) which can take
+        //   either double type value in one method or
+        //   String of double value like "2000.0"
+        // - Create account1 and account2 object instances
+        // - Write logic of withdrawing some amount
+        //   of money from account1 and deposit to
+        //   account2 in this main method
+    }
+}
