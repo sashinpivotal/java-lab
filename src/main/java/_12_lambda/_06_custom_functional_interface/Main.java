792b8d167996f3ae4f1549689879a985ecdcb77e
Sang Shin <sangshinpassion@gmail.com>
2024-07-25 14:08:23 -0400

    lambda

--- src/main/java/_12_lambda/_06_custom_functional_interface/Main.java
+++ src/main/java/_12_lambda/_06_custom_functional_interface/Main.java
@@ -3,13 +3,13 @@ package _12_lambda._06_custom_functional_interface;
 class Main {
     public static void main(String[] args) {
 
-        // TODO-03:
+        // TODO-custom-fi-01:
         // - Use anonymous inner class to create an
         //   object of MyFunctionalInterfaceLower type
         // - Invoke the method of the functional
         //   interface and display the result
 
-        // TODO-04:
+        // TODO-custom-fi-02:
         // - Use Lambda to create an object of
         //   MyFunctionalInterfaceLower type
         // - Invoke the method of the functional
