401f2e6f60335ae526ab39e20149e295c776039c
Sang Shin <sangshinpassion@gmail.com>
2024-07-25 14:14:40 -0400

    lambda

--- src/main/java/_12_lambda/_05_custom_functional_interface/Main.java
+++ src/main/java/_12_lambda/_05_custom_functional_interface/Main.java
@@ -3,6 +3,21 @@ package _12_lambda._05_custom_functional_interface;
 class Main {
     public static void main(String[] args) {
 
+        // Use anonymous inner class
+        MyFunctionalInterfaceUpper myObject1
+                = new MyFunctionalInterfaceUpper() {
+            @Override
+            public String makeUpperCase(String string) {
+                return string.toUpperCase();
+            }
+        };
+        System.out.println(myObject1.makeUpperCase("Boston"));
+
+        // Use Lambda
+        MyFunctionalInterfaceUpper myObject2
+                = string -> string.toUpperCase();
+        System.out.println(myObject2.makeUpperCase("Boston"));
+
         // TODO-custom-fi-01:
         // - Use anonymous inner class to create an
         //   object of MyFunctionalInterfaceLower type
