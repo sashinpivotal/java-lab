b4d68a7557eeab45f3a080234ead020efb879387
Sang Shin <sangshinpassion@gmail.com>
2024-07-25 14:03:43 -0400

    lambda change

--- src/main/java/_12_lambda/_03_jdk_functional_interface/supplier/Main.java
+++ src/main/java/_12_lambda/_03_jdk_functional_interface/supplier/Main.java
@@ -6,10 +6,19 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// TODO-lambda-functional-interface-04a:
+		// - Study how lambda object of
+		//   Supplier<String> functional interface type
+		//   is defined and invoked below
 		Supplier<String> supplier1 = () -> "Boston";
 		String result = supplier1.get();
 		System.out.println(result);
-		
+
+		// TODO-lambda-functional-interface-04b:
+		// - Study how lambda object of
+		//   Supplier<String> functional interface type
+		//   is defined and passed to a method
+		//   as an argument
 		Supplier<Integer> supplier2 = () -> 34;
 		printSupplied(supplier2);
 
