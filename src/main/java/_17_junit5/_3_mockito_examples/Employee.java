8eaf2c0792151e9e9f1fa421af31f506faa99bbd
Sang Shin <sangshinpassion@gmail.com>
2024-07-26 10:14:33 -0400

    mockito example added

---
+++ src/main/java/_17_junit5/_3_mockito_examples/Employee.java
@@ -0,0 +1,21 @@
+package _17_junit5._3_mockito_examples;
+
+public class Employee {
+
+    private String name;
+    private Address address;
+
+    public Employee(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUpperCasedPersonInfo() {
+        String info = getName() + " lives in " + address.getInfo();
+        return info.toUpperCase();
+    }
+}
