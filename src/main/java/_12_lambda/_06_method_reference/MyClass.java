5f07e079dba1dca2ec13feb29ab5b5a243af781f
Sang Shin <sangshinpassion@gmail.com>
2024-07-26 10:22:17 -0400

    method reference

--- src/main/java/_12_lambda/_06_method_reference/MyClass.java
+++ src/main/java/_12_lambda/_06_method_reference/MyClass.java
@@ -1,12 +1,17 @@
 package _12_lambda._06_method_reference;
 
-// Let's say you have defined a functional interface @FunctionalInterface
+// TODO-method-reference-01
+// - Study the following code reading the comments
+
+// Let's say you have defined a functional
+// interface @FunctionalInterface
 interface MyFunctionalInterface {
     public void mySingleAbstractMethod(String name, int age);
 
 }
 
-// Let's also assume there is an existing static method in a class
+// Let's also assume there is an existing
+// static method in a class
 public class MyClass {
     public static void existingStaticMethod(String name, int age) {
         System.out.println(name + " is " + age + " years old.");
@@ -15,11 +20,15 @@ public class MyClass {
 
 class Main {
     public static void main(String[] args) {
-        // You would write a lambda expression as following if you want to use the existing static method of the class
+
+        // You would write a lambda expression as following
+        // if you want to use the existing static method of the class
         MyFunctionalInterface myObject2
                 = (name, age) -> MyClass.existingStaticMethod(name, age);
         myObject2.mySingleAbstractMethod("Jon", 77);
-        // The above lambda expression can be rewritten using a static method reference
+
+        // The above lambda expression can be rewritten
+        // using a static method reference
         MyFunctionalInterface myObject3
                 = MyClass::existingStaticMethod;
         myObject3.mySingleAbstractMethod("Jon", 66);
